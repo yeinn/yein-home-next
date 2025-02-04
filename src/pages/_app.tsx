5389435c0e9efe9d2c79a133e7302abaa142e932
Yein Choi <54708042+yeinn@users.noreply.github.com>
2025-02-05 02:00:20 +0900

    downgrade next, react for vercel
    next, react version downgrade
    next 15 => 14
    react 19 => 18

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 "use client";
 
-import "@/styles/globals.css"; // Tailwind CSS 적용
-import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
       <>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>예인이네</title>
+        </Head>
         <Component {...pageProps} />
       </>
   );
